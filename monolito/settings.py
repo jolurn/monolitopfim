5c0785263264045c5daeccc6e182b394506a2b6c
jolurn <jolurn7@gmail.com>
2023-04-13 11:39:45 -0500

    ver error por debug

--- monolito/settings.py
+++ monolito/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG=True
 # DEBUG = 'RENDER' not in os.environ
 
-# ALLOWED_HOSTS = []
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # pymysql.install_as_MySQLdb()
 # Application definition
